ca230df00ebc152e04a9dd6abde66115f0e64d31
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:07:39 +0100

    Add rule repository

---
+++ Source/Our.Umbraco.HideProperties/Models/Repositories/RuleRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace Our.Umbraco.HideProperties.Models.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Persistence;
+    using global::Umbraco.Core.Persistence.SqlSyntax;
+
+    using Our.Umbraco.HideProperties.Constants;
+    using Our.Umbraco.HideProperties.Models.Pocos;
+
+    internal class RuleRepository
+    {
+        public static readonly RuleRepository Current = new RuleRepository();
+
+        public Database Database => ApplicationContext.Current.DatabaseContext.Database;
+
+        public ISqlSyntaxProvider SqlSyntax => ApplicationContext.Current.DatabaseContext.SqlSyntax;
+
+        public IEnumerable<Rule> Get()
+        {
+            var query = new Sql().Select("*").From(TableConstants.Rules.TableName);
+
+            return this.Database.Fetch<Rule>(query);
+        }
+
+        public Rule GetByKey(int key)
+        {
+            var query = new Sql().Select("*").From(TableConstants.Rules.TableName).Where<Rule>(x => x.Key.Equals(key), this.SqlSyntax);
+
+            return this.Database.Fetch<Rule>(query).FirstOrDefault();
+        }
+
+        public Rule Save(Rule rule)
+        {
+            if (rule != null)
+            {
+                if (rule.Key != Guid.Empty)
+                {
+                    this.Database.Update(rule);
+                }
+                else
+                {
+                    rule.Key = Guid.NewGuid();
+                    this.Database.Save(rule);
+                }
+            }
+
+            return rule;
+        }
+    }
+}
