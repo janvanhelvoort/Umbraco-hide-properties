bfcdc3b770188d35c5bfae00e22acb6f42d9b8da
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:12:40 +0100

    Add start-up class

---
+++ Source/Our.Umbraco.HideProperties/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace Our.Umbraco.HideProperties
+{
+    using global::Umbraco.Core;
+    public class Startup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            using (ApplicationContext.Current.ProfilingLogger.TraceDuration<Startup>("Begin ApplicationStarted", "End ApplicationStarted"))
+            {
+                base.ApplicationStarted(umbracoApplication, applicationContext);
+            }
+        }
+    }
+}
