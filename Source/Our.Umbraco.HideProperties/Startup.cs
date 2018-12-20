3c62a2c94af18df2e90c4546ed62d69610156568
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:16:23 +0100

    Add server variables

--- Source/Our.Umbraco.HideProperties/Startup.cs
+++ Source/Our.Umbraco.HideProperties/Startup.cs
@@ -1,15 +1,26 @@
 ﻿namespace Our.Umbraco.HideProperties
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     using AutoMapper;
+
     using global::Umbraco.Core;
     using global::Umbraco.Core.Logging;
     using global::Umbraco.Core.Persistence.Migrations;
     using global::Umbraco.Web;
+    using global::Umbraco.Web.UI.JavaScript;
+
     using Our.Umbraco.HideProperties.Constants;
+    using Our.Umbraco.HideProperties.Controllers.ApiControllers;
     using Our.Umbraco.HideProperties.Mapping.Profile;
+
     using Semver;
+
     public class Startup : ApplicationEventHandler
     {
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
@@ -21,6 +32,8 @@
                 this.SetupMigration();
 
                 Mapper.AddProfile<RuleProfile>();
+
+                ServerVariablesParser.Parsing += this.ServerVariablesParserParsing;
             }
         }
 
@@ -49,6 +62,27 @@
                 {
                     LogHelper.Error<Startup>("Error running Statistics migration", e);
                 }
+            }
+        }
+
+        private void ServerVariablesParserParsing(object sender, Dictionary<string, object> e)
+        {
+            if (HttpContext.Current == null)
+            {
+                throw new InvalidOperationException("HttpContext is null");
+            }
+
+            if (!e.Keys.Contains("hideProperties"))
+            {
+                var urlHelper = new UrlHelper(new RequestContext(new HttpContextWrapper(HttpContext.Current), new RouteData()));
+
+                var urlDictionairy = new Dictionary<string, object>
+                {
+                    { "getRules", urlHelper.GetUmbracoApiService<RulesApiController>("Get") },
+                    { "saveRule", urlHelper.GetUmbracoApiService<RulesApiController>("Post") }
+                };
+
+                e.Add("hideProperties", urlDictionairy);
             }
         }
     }
