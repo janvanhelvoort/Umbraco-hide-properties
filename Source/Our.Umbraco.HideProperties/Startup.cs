93a3c24a643cb52cfb749a863986997dae705e0a
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:14:08 +0100

    Setup Migration

--- Source/Our.Umbraco.HideProperties/Startup.cs
+++ Source/Our.Umbraco.HideProperties/Startup.cs
@@ -1,6 +1,13 @@
 ﻿namespace Our.Umbraco.HideProperties
 {
+    using System;
+    using System.Linq;
     using global::Umbraco.Core;
+    using global::Umbraco.Core.Logging;
+    using global::Umbraco.Core.Persistence.Migrations;
+    using global::Umbraco.Web;
+    using Our.Umbraco.HideProperties.Constants;
+    using Semver;
     public class Startup : ApplicationEventHandler
     {
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
@@ -8,6 +15,36 @@
             using (ApplicationContext.Current.ProfilingLogger.TraceDuration<Startup>("Begin ApplicationStarted", "End ApplicationStarted"))
             {
                 base.ApplicationStarted(umbracoApplication, applicationContext);
+
+                this.SetupMigration();
+            }
+        }
+
+        private void SetupMigration()
+        {
+            var migrations = ApplicationContext.Current.Services.MigrationEntryService.GetAll(ApplicationConstants.ProductName);
+            var latestMigration = migrations.OrderByDescending(x => x.Version).FirstOrDefault();
+
+            var currentVersion = latestMigration != null ? latestMigration.Version : new SemVersion(0, 0, 0);
+
+            var targetVersion = new SemVersion(0, 1, 0);
+            if (targetVersion != currentVersion)
+            {
+                var migrationsRunner = new MigrationRunner(
+                    ApplicationContext.Current.Services.MigrationEntryService,
+                    ApplicationContext.Current.ProfilingLogger.Logger,
+                    currentVersion,
+                    targetVersion,
+                    ApplicationConstants.ProductName);
+
+                try
+                {
+                    migrationsRunner.Execute(UmbracoContext.Current.Application.DatabaseContext.Database);
+                }
+                catch (Exception e)
+                {
+                    LogHelper.Error<Startup>("Error running Statistics migration", e);
+                }
             }
         }
     }
