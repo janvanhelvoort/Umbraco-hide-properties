b4c9bc60cfa25d63676c43909bc26c43f9a51192
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:09:59 +0100

    Add rule api controller

---
+++ Source/Our.Umbraco.HideProperties/Attributes/CamelCaseAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace Our.Umbraco.HideProperties.Attributes
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http.Formatting;
+    using System.Web.Http.Controllers;
+
+    using Newtonsoft.Json.Serialization;
+
+    public class CamelCaseAttribute : Attribute, IControllerConfiguration
+    {
+        public void Initialize(HttpControllerSettings currentConfiguration, HttpControllerDescriptor currentDescriptor)
+        {
+            var currentFormatter = currentConfiguration.Formatters.OfType<JsonMediaTypeFormatter>().Single();
+
+            // remove the current formatter
+            currentConfiguration.Formatters.Remove(currentFormatter);
+
+            var camelFormatter = new JsonMediaTypeFormatter
+            {
+                SerializerSettings = { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+            };
+
+            // add the camel case formatter
+            currentConfiguration.Formatters.Add(camelFormatter);
+        }
+    }
+}
---
+++ Source/Our.Umbraco.HideProperties/Controllers/ApiControllers/RulesApiController.cs
@@ -0,0 +1,42 @@
+﻿namespace Our.Umbraco.HideProperties.Controllers.ApiControllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using global::Umbraco.Web.Editors;
+
+    using Our.Umbraco.HideProperties.Attributes;
+    using Our.Umbraco.HideProperties.Models;
+    using Our.Umbraco.HideProperties.Services;
+
+    [CamelCase]
+    public class RulesApiController : UmbracoAuthorizedJsonController
+    {
+        public object QuestionRepository { get; private set; }
+
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK, RuleService.Current.GetRules());
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Rule rule)
+        {
+            using (var transaction = this.ApplicationContext.DatabaseContext.Database.GetTransaction())
+            {
+                rule = RuleService.Current.Save(rule);
+
+                if (rule != null)
+                {
+                    transaction.Complete();
+
+                    return this.Request.CreateResponse(HttpStatusCode.OK, rule);
+                }
+
+                return this.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Can't save rule");
+            }                
+        }
+    }
+}
