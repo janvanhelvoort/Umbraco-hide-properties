24c404c6222b2f6f5d6ccc4f89cca751400ef782
Jan van Helvoort <jan@qvision.nl>
2018-12-20 21:09:14 +0100

    Add rule service

---
+++ Source/Our.Umbraco.HideProperties/Services/RuleService.cs
@@ -0,0 +1,52 @@
+﻿namespace Our.Umbraco.HideProperties.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using global::Umbraco.Core;
+    using Our.Umbraco.HideProperties.Constants;
+    using Our.Umbraco.HideProperties.Models;
+    using Our.Umbraco.HideProperties.Models.Repositories;
+
+    public class RuleService
+    {
+        public static readonly RuleService Current = new RuleService();
+
+        public IEnumerable<Rule> Rules { get
+            {
+
+                return Mapper.Map<IEnumerable<Rule>>(RuleRepository.Current.Get());
+            }
+        }
+
+        public IEnumerable<Rule> GetRules()
+        {
+            return this.Rules;
+        }
+
+        public IEnumerable<Rule> GetRules(string alias)
+        {
+            return this.Rules.Where(rule => rule.ContentTypeAlias.Equals(alias));
+        }
+
+        public IEnumerable<Rule> GetActiveRules(string alias)
+        {
+            return this.Rules.Where(rule => rule.IsActive && rule.ContentTypeAlias.Equals(alias));
+        }
+
+        public IEnumerable<Rule> GetActiveRules(IEnumerable<string> aliases)
+        {
+            return this.Rules.Where(rule => rule.IsActive && aliases.Contains(rule.ContentTypeAlias));
+        }
+
+        public Rule Save(Rule rule)
+        {
+            var poco = Mapper.Map<Models.Pocos.Rule>(rule);
+
+            poco = RuleRepository.Current.Save(poco);
+
+            return Mapper.Map<Rule>(poco);
+        }
+    }
+}
