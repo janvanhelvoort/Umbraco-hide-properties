f72ad40fcfef37abc8fc1c690f7e388b8a540e36
Jan van Helvoort <jan@qvision.nl>
2018-12-21 09:28:01 +0100

    Add http resource for get or save rules

---
+++ Source/Our.Umbraco.HideProperties/Client/resources/hide-properties.resource.js
@@ -0,0 +1,13 @@
+﻿function HidePropertiesResource($http) {
+    return {
+        // Rules
+        getRules: function () {
+            return $http.get(Umbraco.Sys.ServerVariables.hideProperties.getRules);
+        },      
+        saveRule: function (rule) {
+            return $http.post(Umbraco.Sys.ServerVariables.hideProperties.saveRule, rule);
+        }
+    };
+};
+
+angular.module("umbraco.resources").factory("hidePropertiesResource", HidePropertiesResource)
