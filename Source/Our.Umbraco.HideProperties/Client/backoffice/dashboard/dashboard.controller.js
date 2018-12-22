85db06afc86924c5dd12fade18cbe99d2c1626f4
Jan van Helvoort <janvanhelvoort@outlook.com>
2018-12-22 16:59:28 +0100

    Add option to select rules

--- Source/Our.Umbraco.HideProperties/Client/backoffice/dashboard/dashboard.controller.js
+++ Source/Our.Umbraco.HideProperties/Client/backoffice/dashboard/dashboard.controller.js
@@ -7,7 +7,25 @@
         $scope.isLoading = false;
     });
 
-    $scope.openRuleBuilderOverlay = function (rule) {        
+    $scope.selectItem = function (rule, $event) {
+        rule.isSelected = !rule.isSelected;
+
+        $event.stopPropagation();
+    };
+
+    $scope.selectAll = function ($event) {
+        angular.forEach($scope.content.rules, function (rule, index) {
+            rule.isSelected = $event.target.checked;
+        });
+
+        $event.stopPropagation();
+    };
+
+    $scope.isSelectedAll = function () {
+        return $scope.content.rules.every(function (rule) { return rule.isSelected });
+    };
+
+    $scope.openRuleBuilderOverlay = function (rule) {
         $scope.ruleBuilderOverlay = {
             view: "../App_Plugins/Umbraco-hide-properties/backoffice/overlays/ruleBuilder/ruleBuilder.overlay.html",
             title: localizationService.localize("hideProperties_ruleOverlay"),
@@ -15,22 +33,22 @@
             initialRule: rule,
             hideSubmitButton: false,
             submit: function (model) {
-                hidePropertiesResource.saveRule(model.rule).then(function(result) {
-                    if(model.rule.key){
-                        angular.forEach($scope.content.rules, function(rule, index){
-                            if(rule.key === result.data.key) {
+                hidePropertiesResource.saveRule(model.rule).then(function (result) {
+                    if (model.rule.key) {
+                        angular.forEach($scope.content.rules, function (rule, index) {
+                            if (rule.key === result.data.key) {
                                 $scope.content.rules[index] = result.data;
                             }
-                        }); 
+                        });
                     } else {
                         $scope.content.rules.push(result.data);
                     }
                 })
-            
+
                 $scope.ruleBuilderOverlay.show = false;
                 $scope.ruleBuilderOverlay = null;
             }
-        };        
+        };
     };
 }
 
