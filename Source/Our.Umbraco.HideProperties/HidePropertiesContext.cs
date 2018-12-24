5e5ce82754f6f4b36624632954709c872184d3c8
Jan van Helvoort <janvanhelvoort@outlook.com>
2018-12-24 16:08:54 +0100

    Move settings from context to config class

--- Source/Our.Umbraco.HideProperties/HidePropertiesContext.cs
+++ Source/Our.Umbraco.HideProperties/HidePropertiesContext.cs
@@ -1,9 +1,5 @@
 ﻿namespace Our.Umbraco.HideProperties
 {
-    using System.Configuration;
-
-    using global::Umbraco.Core;
-
     /// <summary>
     /// The Hide-properties Context
     /// </summary>
@@ -19,6 +15,7 @@
         /// </summary>
         private HidePropertiesContext()
         {
+            this.Configuration = HidePropertiesConfig.Current;            
             instance = this;
         }
 
@@ -27,27 +24,7 @@
         /// </summary>
         public static HidePropertiesContext Current => instance ?? new HidePropertiesContext();
 
-        /// <summary>
-        /// Gets the value of app setting 
-        /// </summary>
-        /// <param name="key">The key.</param>
-        /// <typeparam name="T">The return type</typeparam>
-        /// <returns>The <see cref="T"/>.</returns>
-        private T GetAppSetting<T>(string key, T defaultValue)
-        {
-            var setting = ConfigurationManager.AppSettings[key];
-
-            if (setting != null)
-            {
-                var attempConvert = setting.TryConvertTo<T>();
-
-                if (attempConvert.Success)
-                {
-                    return attempConvert.Result;
-                }
-            }
-
-            return defaultValue;
-        }
+        // Configuration
+        public HidePropertiesConfig Configuration { get; set; }
     }
 }
